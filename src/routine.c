9e77c7fb03e15f1a84487663818c3187ab2c8008
Olivier Roy <120348668+oroy-dev@users.noreply.github.com>
2023-10-27 23:27:29 -0400

    Yo

--- src/routine.c
+++ src/routine.c
@@ -6,7 +6,7 @@
 /*   By: olivierroy <olivierroy@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 12:50:42 by oroy              #+#    #+#             */
-/*   Updated: 2023/10/26 23:09:50 by olivierroy       ###   ########.fr       */
+/*   Updated: 2023/10/27 23:22:27 by olivierroy       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ void	sleeping(t_philo *philo)
 {
 	printf ("%i is sleeping\n", philo->id);
 	usleep(philo->rules->time_to_sleep);
+	printf ("%i is thinking\n", philo->id);
 	philo->state = THINKING;
 }
 
@@ -23,33 +24,28 @@ void	eating(t_philo *philo)
 {
 	printf ("%i is eating\n", philo->id);
 	usleep(philo->rules->time_to_eat);
-	philo->fork->status = AVAILABLE;
 	pthread_mutex_unlock (&philo->fork->mutex);
-	philo->fork->next->status = AVAILABLE;
 	pthread_mutex_unlock (&philo->fork->next->mutex);
+	philo->fork->status = AVAILABLE;
+	philo->fork->next->status = AVAILABLE;
 	philo->fork_count = 0;
 	philo->state = SLEEPING;
 }
 
 void	thinking(t_philo *philo)
 {
-	printf ("%i is thinking\n", philo->id);
-	if (philo->fork->status == AVAILABLE)
+	if (philo->fork->status == AVAILABLE && philo->fork->next->status == AVAILABLE)
 	{
-		printf ("%i has taken a fork\n", philo->id);
 		philo->fork->status = TAKEN;
-		pthread_mutex_lock (&philo->fork->mutex);
-		philo->fork_count++;
-	}
-	if (philo->fork->status == TAKEN && philo->fork->next->status == AVAILABLE)
-	{
-		printf ("%i has taken a fork\n", philo->id);
 		philo->fork->next->status = TAKEN;
+		pthread_mutex_lock (&philo->fork->mutex);
 		pthread_mutex_lock (&philo->fork->next->mutex);
-		philo->fork_count++;
-	}
-	if (philo->fork_count == 2)
+		printf ("%i has taken a fork\n", philo->id);
+		printf ("%i has taken a fork\n", philo->id);
 		philo->state = EATING;
+	}
+	// if (philo->fork_count == 2)
+	// 	philo->state = EATING;
 }
 
 void	*routine(void *arg)
@@ -59,6 +55,7 @@ void	*routine(void *arg)
 
 	philo = (t_philo *)arg;
 	// countdown = philo->rules->time_to_die;
+	printf ("%i is thinking\n", philo->id);
 	while (philo->state != DEAD)
 	{
 		if (philo->state == THINKING)
